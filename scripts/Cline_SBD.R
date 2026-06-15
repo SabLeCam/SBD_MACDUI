7d84c8c371d4b431bb35e0118a844741f86cb2e2
SabLeCam <sablecam15@gmail.com>
2026-06-15 14:53:31 +0200

    Update Cline_SBD.R

--- scripts/Cline_SBD.R
+++ scripts/Cline_SBD.R
@@ -6,19 +6,18 @@ library(parallel)
 library(extrafont)
 library(coda)
 
-##############################################################################
-### 
+################################################################################# 
 
 ### prepare data
-setwd("~/Documents/MAC_DUI/SBD_MS/analyses_eric/clines/hzar_analyses/")
+setwd("path_to_datafile")
 read.csv("../hapgp_sab.txt",header=T, sep="\t") -> macMolecular
 macMolecular
 
 
-#setwd("~/Documents/MAC_DUI/SBD_MS/analyses_eric/clines/")
-##########TEST ONLY FEW SIMPLE MODELS BECAUSE FEW POINTS####################################################################
-### prep hzar analysis
-
+##################################################
+##### Cline analyses #############################
+##################################################
+#based on supplementary information R script examples from Derryberry et al 2013
 
 ## A typical chain length. This value is the default setting in the package.
 chainLength=1e5
@@ -47,9 +46,8 @@ if(require(doMC)){
 macMolecular
 
 ## ## Picking an allele for a locus
-#useAlleles <- "prop_b1a";
-#ua.nSamples <- "cox1.nSamples"
-locnames<-"b1a_f"
+
+locnames<-"b1a_f"    #  do that for all haplogroups
 hap_data<-macMolecular$b1a_f
 hap_nsamples<-macMolecular$f_nSamples
 
@@ -87,16 +85,7 @@ mac.loadmodel("none","none","model1");
 mac.loadmodel("fixed" ,"none","model6");
 
 
-## Check the default settings
-#print(mac$hap$models)
-
-#save output
-#checkdefaultsettings<-capture.output(print(mac$hap$models))
-
-#cat(checkdefaultsettings,file=paste0(locnames,"_checkdefaultsettings.txt"),sep="",append=TRUE)
-
-## Modify all models to focus on the region where the observed data were collected.
-## Observations were between 0 and1511 km
+## Observations were between 0 and 1511 km
 mac$hap$models <- sapply(mac$hap$models,
                          hzar.model.addBoxReq,
                          0 , 1600,
@@ -149,23 +138,9 @@ mac$hap$runs$init <-
 mac$hap$runs$init$model1 <-
   hzar.doFit(mac$hap$fitRs$init$model1)
 
-#plot(hzar.mcmc.bindLL(mac$hap$runs$init$model1))
-## Plot the trace
-#windows()
-#png(width=900, height=900, res=200, family="Arial", filename=paste0(locnames,"_tracemodel_1",".png"),pointsize=8)
-#plot(hzar.mcmc.bindLL(mac$hap$runs$init$model1))
-#dev.off()
-
 ## Run another model for an initial chain
 mac$hap$runs$init$model6 <-
   hzar.doFit(mac$hap$fitRs$init$model6)
-
-## Plot the trace
-#windows()
-#png(width=900, height=900, res=200, family="Arial", filename=paste0(locnames,"_tracemodel_6",".png"),pointsize=8)
-#plot(hzar.mcmc.bindLL(mac$hap$runs$init$model6))
-#dev.off()
-
 
 ## Compile a new set of fit requests using the initial chains
 mac$hap$fitRs$chains <-
@@ -177,8 +152,6 @@ mac$hap$fitRs$chains <-
   hzar.multiFitRequest(mac$hap$fitRs$chains,
                        each=3,
                        baseSeed=NULL)
-
-## Just to be thorough, randomize the initial value for each fit
 
 
 # Get parameters in each model.
@@ -223,14 +196,6 @@ Checkmodel_1_convergence<-capture.output(print(summary(do.call(mcmc.list,
 cat(Checkmodel_1_convergence,file=paste0(locnames,"_Check_model_1_convergence.txt"),sep="",append=TRUE)
 
 
-#mac$hap$fitRs$extra.model1 <- lapply(lapply(mac$hap$runs$chains[1:3], function(x) x[[3]]), # use 3rd run from chain
-#               hzar.next.fitRequest)
-
-## Only do a single run.
-#mac$hap$runs$extra.modelI <- hzar.doFit.multi(mac$hap$fitRs$extra.model1, doPar=TRUE, inOrder=FALSE)
-## Double check, are these three runs also convergent?
-#summary(do.call(mcmc.list,lapply(mac$hap$runs$extra.model1,hzar.mcmc.bindLL)))
-
 ## Did model6 converge?
 summary(do.call(mcmc.list,
                 lapply(mac$hap$runs$chains[4:6],
@@ -251,40 +216,6 @@ Checkmodel_6_convergence<-capture.output(print(summary(do.call(mcmc.list,
 
 cat(Checkmodel_6_convergence,file=paste0(locnames,"_Check_model_6_convergence.txt"),sep="",append=TRUE)
 
-
-## Request an additonal run for each chain of model6.  These
-## three runs will be included in the analysis stage.
-
-#mac$hap$fitRs$extra.model6 <-
-#  lapply(lapply(mac$hap$runs$chains[4:6], # Work from modelIII chains
-#                function(x) x[[3]]),        # Use third run from chain
-#         hzar.next.fitRequest)             
-
-## Reduce the tune value as the parameters are near convergence.
-
-#mac$hap$fitRs$extra.model6[[1]]$modelParam$tune <- 
-#  as.list(as.numeric(mac$hap$fitRs$extra.model6[[1]]$modelParam$tune)*0.9)
-
-
-#mac$hap$fitRs$extra.model6[[2]]$modelParam$tune <- 
-#  as.list(as.numeric(mac$hap$fitRs$extra.model6[[1]]$modelParam$tune)*0.9)
-
-
-#mac$hap$fitRs$extra.model6[[3]]$modelParam$tune <- 
-#  as.list(as.numeric(mac$hap$fitRs$extra.model6[[1]]$modelParam$tune)*0.9)
-
-## Only do a single run.
-
-#mac$hap$fitRs$extra.model6 <-
-#  hzar.doFit.multi(mac$hap$fitRs$extra.model6,
-#                   doPar=TRUE,
-#                   inOrder=FALSE)
-
-## Double check, are these three runs convergent?
-#summary(do.call(mcmc.list,
-#                lapply(
-#                  mac$hap$fitRs$extra.model6,
-#                  hzar.mcmc.bindLL )))
 
 ## Start aggregation of data for analysis
 
@@ -309,19 +240,6 @@ mac$hap$analysis$oDG <-
   hzar.copyModelLabels(mac$hap$analysis$initDGs,
                        mac$hap$analysis$oDG)
 
-## Convert all runs to hzar.dataGroup objects, adding them to the hzar.obsDataGroup object.
-#mac$hap$analysis$oDG <-
-#  hzar.make.obsDataGroup(lapply(mac$hap$runs$chains,
-#                                hzar.dataGroup.add),
-#                         mac$hap$analysis$oDG);
-
-#mac$hap$analysis$oDG <-
-#  hzar.make.obsDataGroup(lapply(mac$hap$fitRs$extra.model6,
-#                                hzar.dataGroup.add),
-#                         mac$hap$analysis$oDG);
-
-## Check to make sure that there are only 16 hzar.dataGroup objects in the hzar.obsDataGroup object.
-#print(summary(mac$hap$analysis$oDG$data.groups))
 
 #save output
 Checkdatagroupobjs<-capture.output(print(summary(mac$hap$analysis$oDG$data.groups)))
@@ -361,10 +279,6 @@ cat(SelectedModel,file=paste0(locnames,"_selected_model.txt"),sep="",append=TRUE
 mac$hap$analysis$model.selected <-
   mac$hap$analysis$oDG$data.groups[[mac$hap$analysis$model.name]]
 
-## Look at the variation in parameters for the selected model
-#print(hzar.getLLCutParam(mac$hap$analysis$model.selected,
-#                           names(mac$hap$analysis$model.selected$data.param)));
-
 #save the oupt
 Var_params<-capture.output(print(hzar.getLLCutParam(mac$hap$analysis$model.selected,
                                                     names(mac$hap$analysis$model.selected$data.param))))
@@ -398,114 +312,21 @@ model.selected <- mac$hap$analysis$model.selected
 loc_obj_rds <- paste0(locnames, ".rds")
 saveRDS(model.selected, loc_obj_rds)
 
-
-
-
 ####plots clines####
 
 #load selected models
-hapgp_b1a_m<-readRDS(file = "b1a_m.rds")
-hapgp_b1b_m<-readRDS(file = "b1b_m.rds")
-hapgp_b2_3_m<-readRDS(file = "b2_b3_m.rds")
 
 hapgp_b1a_f<-readRDS(file = "b1a_f.rds")
 hapgp_b1b_f<-readRDS(file = "b1b_f.rds")
 hapgp_b2_3_f<-readRDS(file = "b2_b3_f.rds")
 
 
-hapgp_b1a_a<-readRDS(file = "b1a_a.rds")
-hapgp_b1b_a<-readRDS(file = "b1b_a.rds")
-hapgp_b2_3_a<-readRDS(file = "b2_b3_a.rds")
-
 library("scales") 
 
-hzar.plot.fzCline(hapgp_b2_3_m, fzCol=alpha("#00ff00ff",0.5), main="cline bestfit models for cox1f haplogroups in males")
-hzar.plot.fzCline(hapgp_b1a_m, lty=2, fzCol=alpha("#b3b3b3ff",0.5),pch=1,add=TRUE)
-hzar.plot.fzCline(hapgp_b1b_m, lty=2, fzCol=alpha("#ff6600ff",0.5),pch=2,add=TRUE)
-legend('topright', legend=c("m3", "m2", "m1"),pch=c(3,1,2), cex=0.4)
-
-
-#hzar.plot.obsData(hapgp_b2_3_f, main="cline bestfit models for cox1f haplogroups in females")
+hzar.plot.obsData(hapgp_b2_3_f, main="cline bestfit models for cox1f haplogroups in females")
 hzar.plot.fzCline(hapgp_b1a_f, lty=2, fzCol=alpha("#b3b3b3ff",0.5),pch=1,add=TRUE)
 hzar.plot.fzCline(hapgp_b1b_f, lty=2, fzCol=alpha("#ff6600ff",0.5),pch=2,add=TRUE)
 legend('topright', legend=c("b2/b3", "b1a", "b1b"),pch=c(3,1,2), cex=0.4)
-
-hzar.plot.fzCline(hapgp_b2_3_a, fzCol=alpha("#00ff00ff",0.5), main="cline bestfit models for cox1f haplogroups in males+females")
-hzar.plot.fzCline(hapgp_b1a_a, lty=2, fzCol=alpha("#b3b3b3ff",0.5),pch=1,add=TRUE)
-hzar.plot.fzCline(hapgp_b1b_a, lty=2, fzCol=alpha("#ff6600ff",0.5),pch=2,add=TRUE)
-legend('topright', legend=c("b2/b3", "b1a", "b1b"),pch=c(3,1,2), cex=0.4)
-
-
-hzar.plot.fzCline(hapgp_b1a_f, lty=2, fzCol=rgb(1,0,0, alpha=0.5),pch=22)
-hzar.plot.fzCline(hapgp_b1a_m, lty=2,add=TRUE, fzCol=rgb(0,0,1, alpha=0.5),pch=21)
-#text(b1a_f~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=1)
-text(b1a_m~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-legend('topleft', legend=c("clade b1a, females", "clade b1a, males"),pch=c(3,2), cex=0.9)
-
-hzar.plot.fzCline(hapgp_b1b_f, lty=2, fzCol=rgb(1,0,0, alpha=0.5),pch=22)
-hzar.plot.fzCline(hapgp_b1b_m, lty=2,add=TRUE, fzCol=rgb(0,0,1, alpha=0.5),pch=21)
-#text(b1b_f~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=1)
-text(b1b_m~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-legend('topleft', legend=c("clade b1b, females", "clade b1b, males"),pch=c(3,2), cex=0.9)
-
-
-hzar.plot.fzCline(hapgp_b2_3_m, lty=2, fzCol=rgb(0,0,1, alpha=0.5),pch=21)
-hzar.plot.obsData(mac$hap$obs,add=TRUE, pch=22, bg=rgb(1,0,0, alpha=0.5))
-#text(prop_b2_b3~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=1)
-text(b2_b3_m~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-legend('topleft', legend=c("clade b2/b3, females", "clade b2/b3, males"),pch=c(3,2), cex=0.9)
-
-
-
-####plot diversity vs geographic distance
-
-plot(macMolecular$distance,macMolecular$hd_m, ylim=c(0,1),ylab= "Gene diversity",
-     xlab="Geographic distance from northern sampling point", type = "b")
-lines(macMolecular$distance,macMolecular$hd_f,pch=3, type = "b", lty=2)
-legend('topright', legend=c("cox1m", "cox1f"),pch=c(1,3), cex=0.8, lty=c(1,2))
-text(hd_m~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-
-
-
-
-plot(macMolecular$distance,macMolecular$pi_m, ylim=c(0,1),ylab= "Nucleotide diversity (x10e2)",
-     xlab="Geographic distance from northern sampling point", type = "b")
-lines(macMolecular$distance,macMolecular$pi_f,pch=3, type = "b", lty=2)
-legend('topright', legend=c("cox1m", "cox1f"),pch=c(1,3), cex=0.8, lty=c(1,2))
-text(pi_f~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-
-
-
-macMolecular$nb_sing_F<-c(0, 0.428571429, 0.428571429, 0.8, 0.5,
-                          0.666666667, 0.666666667, 0.5, 0.666666667, 0.714285714,
-                          0.666666667)
-macMolecular$nb_sing_M<-c(0.888888889, 0.272727273,0.785714286,0.555555556,0.857142857,
-                          0.6, 0.833333333, 0.777777778, 0.833333333, 0.833333333,
-                          0.5)
-
-plot(macMolecular$distance,macMolecular$nb_sing_M, ylim=c(0,1),ylab= "proportion of singletons",
-     xlab="Geographic distance from northern sampling point", type = "b")
-lines(macMolecular$distance,macMolecular$nb_sing_F,pch=3, type = "b", lty=2)
-legend('topright', legend=c("cox1m", "cox1f"),pch=c(1,3), cex=0.8, lty=c(1,2))
-text(nb_sing_M~distance, labels=locationID,data=macMolecular, cex=0.6, font=2, pos=3)
-
-
-reg<-lm(macMolecular$hd_f~macMolecular$distance)
-summary(reg)
-anova(reg)
-
-reg<-lm(macMolecular$hd_m~macMolecular$distance)
-summary(reg)
-anova(reg)
-
-
-reg<-lm(macMolecular$pi_f~macMolecular$distance)
-summary(reg)
-anova(reg)
-
-reg<-lm(macMolecular$pi_m~macMolecular$distance)
-summary(reg)
-anova(reg)
 
 
 
